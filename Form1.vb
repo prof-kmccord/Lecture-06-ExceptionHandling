6a0cdfe7222ce4acd35c9a7c082a7e2db519399d
Kenneth McCord <kmccord@vt.edu>
2023-08-29 22:22:17 -0400

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,85 @@
+﻿Public Class Form1
+
+    ' Declare variables for the game logic
+
+    ' Declare variables for score, first number, second number, and answer
+
+
+
+    Private Sub MathGameForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' Initialize the game setup
+
+        ' Populate the ComboBox with arithmetic operations (Addition, Subtraction, Multiplication, Division)
+
+    End Sub
+
+
+
+    Private Sub cboOperation_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboOperation.SelectedIndexChanged
+
+        ' Generate a math question when an operation is selected
+
+        ' Call the GenerateQuestion function
+
+    End Sub
+
+
+
+    Private Sub GenerateQuestion()
+
+        ' Function to generate a random math question based on selected operation
+
+        ' Randomly generate two numbers
+
+
+
+        ' Use a Select Case block to handle the different arithmetic operations
+
+        ' In each case, set the text of lblQuestion to display the math question
+
+    End Sub
+
+
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+
+        ' Exception handling for invalid input
+
+        Try
+
+            ' Get the user's answer from the TextBox and convert it to the necessary data type
+
+
+
+            ' Calculate the correct answer based on the selected operation
+
+
+
+            ' Compare the user's answer to the correct answer
+
+
+
+            ' Update the score and display a message indicating if the answer is correct or not
+
+
+
+        Catch ex As Exception
+
+            ' Handle any exceptions
+
+            ' Display a user-friendly message for invalid input
+
+        Finally
+
+            ' Clear the TextBox for the next question
+
+        End Try
+
+
+
+        ' Generate a new math question for the next round
+
+    End Sub
+
+End Class
